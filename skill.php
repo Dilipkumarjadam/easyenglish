dd0923a6d43b542421b17c55fca66409abf53254
dilip kumar <dilipkumarjadam95@gmail.com>
2023-03-22 22:25:24 +0530

    grammar updated

--- skill.php
+++ skill.php
@@ -11,13 +11,192 @@
         <div class="single_skill">
             <img src="./images/skill.jpg" alt="photo" />
         </div>
-        <div class="single_skill">
-            <img src="./images/grammar.jpg" alt="photo" />
+        <!-- <div class="single_skill">
+            <img src="./images/grammar.webp" alt="photo" />
         </div>
         <div class="single_skill">
-            <img src="./images/men.jpg" alt="photo" />
+            <img src="./images/g1.webp" alt="photo" />
         </div>
+        <div class="single_skill">
+            <img src="./images/g2.jpg" alt="photo" />
+        </div>
+        <div class="single_skill">
+            <img src="./images/g3.webp" alt="photo" />
+        </div> -->
+        <div class="grammar">
+                    <h1>9 English Grammar Rules to Remember</h1>
+                    <h1>One of the most difficult things about learning a new language is learning the grammar rules. And while English grammar can seem quite easy compared to some languages, a small mistake can easily change the meaning of what you want to say.
+So here’s a list of some important rules that you should keep in mind when you speak and write English.</h1>
+                    <h1>1. Adjectives and adverbs</h1>
+<p>Make sure you use adjectives and adverbs correctly. Adjectives describe, identify and quantify people or things and usually go in front of a noun. They don’t change if the noun is plural. Adverbs modify verbs, adjectives and other adverbs and usually come after the verb. For example:</p>
+<li>
+He’s a slow driver. (adjective)
+</li>
+<li>
+He drives slowly. (adverb)
+</li>
+
+<p>Most adverbs are created by adding -ly to an adjective as in the example, but a few adverbs are irregular, such as:
+
+<li>fast (adjective) – fast (adverb)</li>
+
+<li>hard (adjective) – hard (adverb)</li>
+
+<li>good (adjective) – well (adverb)</li>
+
+For example, Your English is good. You speak English well.</p>
+
+
+<h1>2. Pay attention to homophones</h1>
+<p>
+Homophonic words are words that are pronounced in the same way as other words but have different meanings, even if they are spelt differently. This can obviously create confusion and unfortunately there are many of these words in English. For example:
+
+<li>they’re – their – there</li>
+<li>you’re – your</li>
+<li>it’s – its</li>
+<li>I – eye</li>
+<li>here – hear</li>
+<li>break – brake</li>
+<li>flower – flour</li>
+<li>our – hour</li>
+So when you’re writing, be careful to choose the right spelling. And when you listen, remember that a word you think you understood may have another meaning. Try to understand that meaning from the context.
+</p>
+<h1>3. Use the correct conjugation of the verb </h1>
+<p>Remember to change the verb to agree with the subject. The main subjects you need to be careful with are he, she and it because they often have a different form to the others. For example:
+
+<li>She has two cats.  RIGHT</li>
+
+<li>She have two cats. WRONG</li>
+
+This seems like a small mistake to make but unfortunately it’s a very noticeable one. So if you can avoid it, it’ll make a big difference to how accurate you sound.
+
+Remember also that when you describe something using ‘There is/are’, the verb must agree with the first item you mention. For example:
+
+<li>There is a sofa, some chairs and a table.</li>
+
+<li>There are some chairs, a table and a sofa.</li>
+</p>
+<h1>4. Connect your ideas with conjunctions</h1>
+<p>
+If you want to connect two ideas or short phrases, you can do so by using a conjunction. For example,
+
+<li>I’m studying English. English is important.
+
+becomes:
+
+I’m studying English because it’s important.
+
+The most common conjunctions are:
+
+<li>and – addition</li>
+
+<li>because – to give the reason </li>
+
+<li>but – to express contrast</li>
+
+<li>so – to describe a consequence</li>
+
+<li>or – to describe an alternative</li>
+
+Here are some examples:
+
+He likes football and he plays in a team.
+We’re going out because we’re bored.
+She wants to study more but she doesn’t have time.
+Kim is coming round so I’m cleaning my flat.
+Would you like tea or coffee?
+</p>
+
+<h1>5. Sentence construction</h1>
+<p>
+Generally speaking, sentences in written English are not particularly long. This is good news for English learners because it means you don’t need to worry about writing long, complex sentences. A sentence usually has two, or possibly three, clauses (subject + verb + object), linked by a conjunction (see above).
+
+A good way to make your sentences even clearer is to add commas. Commas help the reader understand where one phrase finishes and another begins. The most common occasions where it’s recommended to put a comma are:
+
+between two clauses. For example, If the weather is nice tomorrow, we’re going to the park.
+to separate items in a list. For example, Our kids like swimming, skiing, ice-skating and cycling.
+after some conjunctions. For example, Our holiday was great and the hotel was wonderful. However, the weather was awful.
+for extra information in the middle of a sentence (a non-defining clause). For example, My neighbor, who’s from Brazil, is really good at cooking.
+
+
+And don’t forget to start every sentence with a capital letter!
+</p>
+<h1>6. Remember the word order for questions</h1>
+<p>In English, the structure of questions is different to the affirmative form. So make sure you remember to change the order of the words or add the auxiliary ‘do’. There are four ways to make questions in English:
+
+‘to be’ – for questions using the verb ‘to be’, invert the subject and verb. For example, Are you a student?
+all other verbs – to make questions for all other verbs, add the auxiliary ‘do’. For example, Do they work here?
+modal verbs – to make questions with modal verbs, invert the modal verb and the subject. For example, Can he play the piano?
+auxiliary verbs – for sentences containing an auxiliary verb, like ‘’have’ in the present perfect, invert the auxiliary verb and the subject. For example, Have you seen Bob?
+ 
+
+These rules still apply when you add a question word like what, how, why. For example:
+
+<li>Where are you from?</li>
+
+<li>When can we meet?</li>
+
+<li>Why have they left?</li>
+</p>
+
+<h1>7. Use the right past form of verbs</h1>
+<p>Speaking about the past in English is not particularly difficult. Every subject uses the same word to express the past, so you don’t have to worry about learning six different words as in some languages. However, many verbs are irregular and don’t follow the regular form of adding -ed. You don’t need to know all of these, but try to learn the most common ones (approximately 20). For example,
+
+<li>Go – went   
+
+Have – had
+
+Make – made</li>
+
+For example,
+
+We went to the cinema last Saturday.
+They had a party to celebrate Tom’s birthday.
+I made a cake this morning.
+</p>
+
+<h1>8. Get familiar with the main English verb tenses</h1>
+<p>
+If you’re just starting to learn English, you won’t know all the tenses yet. And that’s fine. Just focus on becoming familiar with the four or five that are used most often. Aim to be able to use these:
+
+Present simple – to describe habits and permanent situations. For example, We live in New York.
+Present continuous – to describe current situations and future plans. For example, I’m meeting John later.
+Past simple – to describe finished past actions. For example, They arrived at 3 p.m.
+Present perfect – to describe past actions connected to now. For example, We’ve finished the reports.
+Will – to describe future actions. For example, I’ll meet you in front of the conference center.
+</p>
+
+<h1>9. Never use a double negative</h1>
+<p>In English there are often two ways to express a negative concept. For example, if you want to say the room is empty, you can say:
+
+There is nothing in the room. OR There isn’t anything in the room.
+
+The words ‘nothing’ and ‘anything’ have the same meaning, but ‘nothing’ is used with an affirmative verb, and ‘anything’ is used with a negative verb.
+
+This rule applies to other words like:
+
+<li>nobody – anybody</li>
+
+<li>none – any</li>
+
+This is also true of the word ‘never’ when you talk about experience. You can say:
+
+He’s never been to the U.S. OR He hasn’t ever been to the U.S.
+
+The meaning is the same but in the second sentence the use of ‘ever’ means you need to make the verb negative.
+
+Learning all these grammar rules obviously takes time and you also need some guidance to be able to put them into practice. The best way to become confident and proficient in using them is to practice in a supportive and fun environment with experienced teachers. Find out more about Our Method now.
+</p>
+
+
+
+                </div>
     </div>
+    <div class="btn_wrapper">
+                    <a href="courses.php?level=intermediate">
+                        <button>Find out more</button>
+                    </a>    
+                    </div>
 
     <div class="footer" id="contact">
         <div class="footer_info">
